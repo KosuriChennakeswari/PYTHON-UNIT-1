01c2b3cc2629cb3b55f547a1a37984bf058114a1
KosuriChennakeswari <chenna7137@gmail.com>
2026-06-18 12:02:57 +0530

    Update and rename Input output to Input output.py

---
+++ TASK-21.PY/Input output.py
@@ -0,0 +1,8 @@
+Sample Input: Enter a number: 25
+Sample Output:
+Square Root = 5.0
+Power = 625.0
+Logarithm = 3.2188758248682006
+Sin = -0.13235175009777303
+Cos = 0.9912028118634736
+Tan = -0.13352640702153587
